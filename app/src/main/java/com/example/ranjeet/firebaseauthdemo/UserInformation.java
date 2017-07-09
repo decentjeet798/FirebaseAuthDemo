9ed76e8e3295f2d714469722766f98c8fbda23e2
Ranjeet <ranjeetkumar798@gmail.com>
2017-07-09 12:25:45 +0530

    malti project Initial commit

---
+++ app/src/main/java/com/example/ranjeet/firebaseauthdemo/UserInformation.java
@@ -0,0 +1,18 @@
+package com.example.ranjeet.firebaseauthdemo;
+
+/**
+ * Created by Ranjeet on 4/15/2017.
+ */
+
+public class UserInformation {
+    public String name;
+    public String address;
+
+    public UserInformation(){
+
+    }
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
